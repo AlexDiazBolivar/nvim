06f0f7445def832e8d5989be5780c0638be50a6e
Alejandro Diaz <alexdiazbolivar@gmail.com>
2022-06-21 15:32:23 +0200

    Telescope -> ripgrep (rg) for find_files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,11 +3,12 @@ if not status_ok then
   return
 end
 
+local builtin = require "telescope.builtin"
 local actions = require "telescope.actions"
+local M = {}
 
 telescope.setup {
   defaults = {
-
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
@@ -23,3 +24,34 @@ telescope.setup {
     },
   },
 }
+
+M.find_files = function()
+  builtin.find_files {
+    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
+    previewer = false
+  }
+end
+
+M.find_config_files = function()
+  local config_dir = vim.env.HOME .. '/.config/nvim'
+  builtin.find_files {
+    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', config_dir },
+    previewer = false
+  }
+end
+
+M.live_grep = function()
+  builtin.live_grep {}
+end
+
+M.file_browser = function()
+  builtin.file_browser {}
+end
+
+M.buffers = function()
+  builtin.buffers {
+    previewer = false
+  }
+end
+
+return M
